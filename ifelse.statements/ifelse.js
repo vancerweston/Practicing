3ad5ce5c7d172a5d509ebc5622801949755b672d
vancerweston <vweston@student.neumont.edu>
2020-01-27 14:33:45 -0700

    created if else statements in ifelse.js

---
+++ ifelse.statements/ifelse.js
@@ -0,0 +1,36 @@
+/* Proceedure of an if statement
+if(true){
+    // execute this code
+}
+
+if (false){
+    // NOT execute this code
+}
+*/
+
+// Looking for a thrill ....
+// .... code to determin thrill
+
+let selectedThrill = "gaming!";
+
+// Pertains to gaming
+if(selectedThrill === "gaming!"){
+    //if thrill is gaming
+    //do something
+    console.log("You selected gaming, what a thrill.");
+} else {
+    //if not gaming
+    //do this
+    console.log("You did NOT select gaming.");
+} 
+
+// Pertains to skiing
+if(selectedThrill === "skiing!"){
+    //if thrill is skiing
+    //do something
+    console.log("You selected skiing, what a thrill.");
+} else {
+    //if not skiing 
+    //do this
+    console.log("You did NOT select skiing.");
+} 
