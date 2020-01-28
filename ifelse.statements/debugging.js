c1007f1a032f54d90f3c8a6bb97c488381660c84
vancerweston <vweston@student.neumont.edu>
2020-01-28 16:24:52 -0700

    added index.html and scripted debugging.js

---
+++ ifelse.statements/debugging.js
@@ -0,0 +1,13 @@
+
+let firstName = "Fred";
+let lastName = "Smith";
+
+function test() {
+    
+if(firstName === 'Fred') {
+    firstName = "Freddy";
+    console.log(firstName + " is awesome.");
+} else {
+    console.log(firstName + " is amazing.");
+}
+}
