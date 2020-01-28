335655ffe6aaf6342d28fbf4cf11776fb3e0b436
vancerweston <vweston@student.neumont.edu>
2020-01-28 12:03:05 -0700

    created nestedifelse.js

---
+++ ifelse.statements/nestedifelse.js
@@ -0,0 +1,29 @@
+// stuff happens to determine thrill seeking is our objective
+//....
+
+let thrill = "gaming!";
+
+//ask user question: Do you want to understand your thrill seeking?
+
+//user output
+let understanding = false;
+
+if(understanding) {
+    // if YES
+    // Dig into the reasons
+    console.log("Digging into the reasons why.");
+} else {
+    // if NO
+    // ask user: Do you feel responsible today?
+    //....
+    let feelingResponsible = false;
+
+    if(feelingResponsible){
+        // if YES
+        // ask user: Is homework done and chores complete.
+        console.log("Checking my homework and chores are complete.");
+    } else {
+        //if NO
+        console.log("Gaming for decompression purposes!");
+    }
+}
