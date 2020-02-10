d38935161bdfa42e1bf26b86c7731e89c897b7e6
vancerweston <vweston@student.neumont.edu>
2020-02-10 12:50:34 -0700

    started learning about loops

---
+++ Loops/forloop.js
@@ -0,0 +1,10 @@
+
+// For Loops
+
+let numTimesTaskRun = 5;
+
+for (let counter = 0; counter < numTimesTaskRun; counter++) {
+    console.log(counter + ' < ' + numTimesTaskRun);
+}
+
+console.log('This does not execute until the for loop is done.');
